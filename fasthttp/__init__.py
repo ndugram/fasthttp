cb21f28223c6a94128e95cf27d879ccaf59cb015
NEFORCEO <pirogartem919@gmail.com>
2026-04-28 16:46:53 +0700

    update fasthttp/__init__.py

--- fasthttp/__init__.py
+++ fasthttp/__init__.py
@@ -2,7 +2,7 @@ from . import status
 from .__meta__ import __version__
 from .app import FastHTTP
 from .dependencies import Depends
-from .middleware import BaseMiddleware, CacheMiddleware, MiddlewareChain, MiddlewareManager
+from .middleware import BaseMiddleware, CacheMiddleware, MiddlewareChain, MiddlewareManager, SessionMiddleware
 from .routing import Router
 
 __all__ = (
@@ -13,6 +13,7 @@ __all__ = (
     "MiddlewareChain",
     "MiddlewareManager",
     "Router",
+    "SessionMiddleware",
     "__version__",
     "status",
 )
