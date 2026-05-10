94a63221a6fa2bda92556087ce5e16919de083eb
NEFORCEO <pirogartem919@gmail.com>
2026-05-10 22:36:55 +0700

    update fasthttp/__init__.py

--- fasthttp/__init__.py
+++ fasthttp/__init__.py
@@ -2,13 +2,15 @@ from . import status
 from .__meta__ import __version__
 from .app import FastHTTP
 from .dependencies import Depends
-from .middleware import BaseMiddleware, CacheMiddleware, MiddlewareChain, MiddlewareManager, SessionMiddleware
+from .middleware import BaseMiddleware, CacheMiddleware, CookieJar, DummyCookieJar, MiddlewareChain, MiddlewareManager, SessionMiddleware
 from .routing import Router
 
 __all__ = (
     "BaseMiddleware",
     "CacheMiddleware",
+    "CookieJar",
     "Depends",
+    "DummyCookieJar",
     "FastHTTP",
     "MiddlewareChain",
     "MiddlewareManager",
