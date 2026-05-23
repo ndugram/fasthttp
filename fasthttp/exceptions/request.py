cdb29c02e6deea44c51558f8bde2bb4cd68a924e
NEFORCEO <pirogartem919@gmail.com>
2026-05-24 00:06:48 +0700

    update request.py

--- fasthttp/exceptions/request.py
+++ fasthttp/exceptions/request.py
@@ -57,7 +57,7 @@ class FastHTTPRequestError(FastHTTPError):
             ]
             | None
         ) = None,
-        **kwargs,
+        **kwargs: object,
     ) -> None:
         super().__init__(
             message=message,
