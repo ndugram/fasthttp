c7db8e3a3f318f5cef7be00441003a48d029649e
NEFORCEO <pirogartem919@gmail.com>
2026-05-23 23:56:06 +0700

    update run_examples.py

--- examples/graphql/run_examples.py
+++ examples/graphql/run_examples.py
@@ -1,11 +1,17 @@
+from examples.graphql.countries_api import (  # type: ignore
+    get_countries,
+)
+from examples.graphql.pokemon import (  # type: ignore
+    get_pokemon_by_name,
+)
+from examples.graphql.rick_and_morty import (  # type: ignore
+    get_character_by_id,
+)
+from examples.graphql.star_wars import (  # type: ignore
+    get_all_films,
+)
 from fasthttp import FastHTTP
 from fasthttp.response import Response
-
-from examples.graphql.countries_api import get_countries, get_country_by_code, search_countries  # type: ignore  # noqa: E402
-from examples.graphql.rick_and_morty import get_characters, get_character_by_id, get_episodes  # type: ignore  # noqa: E402
-from examples.graphql.star_wars import get_all_films, get_film_by_id, get_all_people  # type: ignore  # noqa: E402
-from examples.graphql.pokemon import get_pokemon_by_name, get_pokemon_list  # type: ignore  # noqa: E402
-
 
 app = FastHTTP()
 
