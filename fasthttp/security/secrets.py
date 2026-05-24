b7116dc22a3ffc95901978ba2b5809fd67602310
NEFORCEO <pirogartem919@gmail.com>
2026-05-24 11:49:38 +0700

    update secrets.py

--- fasthttp/security/secrets.py
+++ fasthttp/security/secrets.py
@@ -4,7 +4,9 @@ try:
     from fasthttp._core import mask_cookie as _rs_mask_cookie  # type: ignore
     from fasthttp._core import mask_headers as _rs_mask_headers  # type: ignore
     from fasthttp._core import mask_log_message as _rs_mask_log_message  # type: ignore
-    from fasthttp._core import should_mask_value as _rs_should_mask_value  # type: ignore
+    from fasthttp._core import (
+        should_mask_value as _rs_should_mask_value,  # type: ignore
+    )
     _RUST = True
 except ImportError:
     _RUST = False
