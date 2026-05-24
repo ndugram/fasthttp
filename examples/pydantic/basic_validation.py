085161f12b45979338e83ba6a237bfb37eaafb4e
NEFORCEO <pirogartem919@gmail.com>
2026-05-24 10:55:10 +0700

    update basic_validation.py

--- examples/pydantic/basic_validation.py
+++ examples/pydantic/basic_validation.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 from fasthttp import FastHTTP
+from fasthttp.response import Response
 
 
 class User(BaseModel):
@@ -13,12 +14,12 @@ app = FastHTTP()
 
 
 @app.get(url="https://jsonplaceholder.typicode.com/users/1", response_model=User)
-async def get_user(resp) -> User:
+async def get_user(resp: Response) -> User:
     return resp.json()
 
 
 @app.get(url="https://jsonplaceholder.typicode.com/users", response_model=list[User])
-async def get_all_users(resp) -> list[User]:
+async def get_all_users(resp: Response) -> list[User]:
     return resp.json()
 
 
