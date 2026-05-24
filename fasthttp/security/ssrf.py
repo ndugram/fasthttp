7fd6067241ac5f3b3f8872c1a63989b7c09c65bd
NEFORCEO <pirogartem919@gmail.com>
2026-05-24 11:33:49 +0700

    update ssrf.py

--- fasthttp/security/ssrf.py
+++ fasthttp/security/ssrf.py
@@ -4,11 +4,11 @@ import socket
 from urllib.parse import urlparse
 
 try:
-    from fasthttp._core import (
-        is_local_hostname as _rs_is_local_hostname,
+    from fasthttp._core import (  # type: ignore[import-untyped]
+        is_local_hostname as _rs_is_local_hostname,  # type: ignore[unresolved-import]
     )
-    from fasthttp._core import (
-        is_private_ip as _rs_is_private_ip,
+    from fasthttp._core import (  # type: ignore[import-untyped]
+        is_private_ip as _rs_is_private_ip,  # type: ignore[unresolved-import]
     )
     _RUST = True
 except ImportError:
