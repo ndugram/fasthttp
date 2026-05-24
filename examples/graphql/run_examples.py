caecbe7cafcb63b086a20c46b17f2af60f41c8bd
NEFORCEO <pirogartem919@gmail.com>
2026-05-24 11:32:39 +0700

    update run_examples.py

--- examples/graphql/run_examples.py
+++ examples/graphql/run_examples.py
@@ -1,13 +1,10 @@
-from examples.graphql.countries_api import (  # type: ignore
-    get_countries,
-)
-from examples.graphql.pokemon import (  # type: ignore
+from examples.graphql.pokemon import (
     get_pokemon_by_name,
 )
-from examples.graphql.rick_and_morty import (  # type: ignore
+from examples.graphql.rick_and_morty import (
     get_character_by_id,
 )
-from examples.graphql.star_wars import (  # type: ignore
+from examples.graphql.star_wars import (
     get_all_films,
 )
 from fasthttp import FastHTTP
@@ -16,24 +13,19 @@ from fasthttp.response import Response
 app = FastHTTP()
 
 
-@app.graphql(url="https://countries.trevorblades.com/graphql", tags=["graphql", "countries"])
-async def countries_handler(resp: Response) -> dict:
-    return await get_countries(resp)
-
-
 @app.graphql(url="https://rickandmortyapi.com/graphql", tags=["graphql", "rick-and-morty"])
 async def rick_morty_handler(resp: Response) -> dict:
-    return await get_character_by_id(resp)
+    return await get_character_by_id(resp)  # type: ignore[invalid-await]
 
 
 @app.graphql(url="https://swapi-graphql.netlify.app/.graphql", tags=["graphql", "star-wars"])
 async def star_wars_handler(resp: Response) -> dict:
-    return await get_all_films(resp)
+    return await get_all_films(resp)  # type: ignore[invalid-await]
 
 
 @app.graphql(url="https://graphql-pokemon.vercel.app/", tags=["graphql", "pokemon"])
 async def pokemon_handler(resp: Response) -> dict:
-    return await get_pokemon_by_name(resp)
+    return await get_pokemon_by_name(resp)  # type: ignore[invalid-await]
 
 
 if __name__ == "__main__":
